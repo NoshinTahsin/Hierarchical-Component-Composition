7d4f2e02045d07140b28dbdd4e3d388d78936a90
NoshinTahsin <35559870+NoshinTahsin@users.noreply.github.com>
2022-02-13 01:55:10 +0600

    component for fetching booklist

---
+++ ClassLibraryBookFinder/ClassLibraryBookFinder/BookFinder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassLibraryBookFinder
+{
+    public class BookFinder
+    {
+        public String findBooks(int year)
+        {
+            String books_of_year = "List of books sold in " + year;
+            return books_of_year;
+        }
+    }
+}
